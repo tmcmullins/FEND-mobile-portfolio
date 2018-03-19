b5e5d97b036de79736e44765480fb919e8290051
Tim McMullins <tm.grafx@gmail.com>
2018-03-19 00:14:32 -0700

    Add files via upload

--- views/js/main.js
+++ views/js/main.js
@@ -406,9 +406,10 @@ function updatePositions() {
 
     // document.body.scrollTop is no longer supported in Chrome.
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    
+    var phase;
+
     for (var i = 0; i < items.length; i++) {       
-        var phase = Math.sin((scrollTop / 1250) + (i % 5));
+        phase = Math.sin((scrollTop / 1250) + (i % 5));
         items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     }
 
